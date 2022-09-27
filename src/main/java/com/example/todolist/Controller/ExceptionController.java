2a3b19c98d645ddf98a19f43791ac244dadd160b
parkdoyeon <parkdoyeon3239@gmail.com>
2022-09-27 17:10:09 +0900

    controlleradvice 추가++

--- src/main/java/com/example/todolist/Controller/ExceptionController.java
+++ src/main/java/com/example/todolist/Controller/ExceptionController.java
@@ -1,8 +1,22 @@
 package com.example.todolist.Controller;
 
 
+import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import java.util.NoSuchElementException;
 
 @ControllerAdvice
 public class ExceptionController {
+    @ExceptionHandler(value = NoSuchElementException.class)
+    public ResponseEntity exception(NoSuchElementException error){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.getMessage());
+    }
+    @ExceptionHandler(value = EmptyResultDataAccessException.class)
+    public ResponseEntity exception(EmptyResultDataAccessException error){
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(error.getMessage());
+    }
 }
