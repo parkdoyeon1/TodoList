c4e366403065a73c5870e5459e1cbebac76e9b5d
parkdoyeon <parkdoyeon3239@gmail.com>
2022-09-27 18:51:42 +0900

    CORS 설정

--- src/main/java/com/example/todolist/config/WebMvcConfig.java
+++ src/main/java/com/example/todolist/config/WebMvcConfig.java
@@ -4,7 +4,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
-//https://dev-pengun.tistory.com/entry/Spring-Boot-CORS-%EC%84%A4%EC%A0%95%ED%95%98%EA%B8%B0
+//cors 설정 https://dev-pengun.tistory.com/entry/Spring-Boot-CORS-%EC%84%A4%EC%A0%95%ED%95%98%EA%B8%B0
 @Configuration
 public class WebMvcConfig implements WebMvcConfigurer {
 
