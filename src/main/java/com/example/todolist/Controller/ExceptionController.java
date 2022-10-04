05ebd1f6e63fafc4eed03182f5551f14c1ec4322
parkdoyeon <parkdoyeon3239@gmail.com>
2022-10-04 19:30:00 +0900

    METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION 익셉션 핸들러 추가

--- src/main/java/com/example/todolist/Controller/ExceptionController.java
+++ src/main/java/com/example/todolist/Controller/ExceptionController.java
@@ -1,11 +1,13 @@
 package com.example.todolist.Controller;
 
 import com.example.todolist.exception.BaseException;
+import com.example.todolist.exception.ErrorCode;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
 
 import java.util.NoSuchElementException;
 
@@ -17,5 +19,13 @@ public class ExceptionController {
         return ResponseEntity.status(error.getErrorCode().getStatus())
                 .body(error.getErrorCode().getMessage());
     }
+
+    @ExceptionHandler(value = MethodArgumentTypeMismatchException.class)
+    public ResponseEntity exception(){
+        ErrorCode errorCode =ErrorCode.METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION;
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(errorCode.getMessage());
+    }
+
 
 }
