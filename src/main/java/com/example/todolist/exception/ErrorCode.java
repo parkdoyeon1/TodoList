dd7ae3b3dec6a17da379c6fcb7504a727dcad8d6
parkdoyeon <parkdoyeon3239@gmail.com>
2022-10-04 17:00:50 +0900

    에러메세지 수정

--- src/main/java/com/example/todolist/exception/ErrorCode.java
+++ src/main/java/com/example/todolist/exception/ErrorCode.java
@@ -4,8 +4,8 @@ import lombok.Getter;
 
 @Getter
 public enum ErrorCode {
-    NOT_FOUND(404,"Not Found Id"),
-    BAD_REQUEST(400,"Bad Request");
+    NOT_FOUND(404,"Not Found Id : 없는 아이디 입니다."),
+    BAD_REQUEST(400,"Bad Request : 잘못된 입력입니다.");
 
     private Integer status;
     private String message;
