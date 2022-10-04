1176d65080fa61ab99510f0896a6db53c1770c33
parkdoyeon <parkdoyeon3239@gmail.com>
2022-10-04 20:11:35 +0900

    exception 핸들러 추가

--- src/main/java/com/example/todolist/Controller/ExceptionController.java
+++ src/main/java/com/example/todolist/Controller/ExceptionController.java
@@ -5,6 +5,9 @@ import com.example.todolist.exception.ErrorCode;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.web.HttpMediaTypeException;
+import org.springframework.web.HttpMediaTypeNotSupportedException;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.method.annotation.MethodArgumentTypeMismatchException;
@@ -26,6 +29,18 @@ public class ExceptionController {
         return ResponseEntity.status(errorCode.getStatus())
                 .body(errorCode.getMessage());
     }
+    @ExceptionHandler(value = HttpMediaTypeNotSupportedException.class)
+    public ResponseEntity unsupportedTypeException() {
+        ErrorCode errorCode = ErrorCode.HTTP_MEDIA_TYPE_NOT_SUPPORTED_EXCEPTION;
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(errorCode.getMessage());
+    }
 
+    @ExceptionHandler(value = HttpMessageNotReadableException.class)
+    public ResponseEntity badRequest(){
+        ErrorCode errorCode = ErrorCode.HTTP_MESSAGE_NOT_READABLE_EXCEPTION;
+        return ResponseEntity.status(errorCode.getStatus())
+                .body(errorCode.getMessage());
+    }
 
 }
