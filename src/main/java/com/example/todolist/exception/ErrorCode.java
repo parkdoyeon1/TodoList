347bbe951de8460c4fd5eac4a03c6b68f40b8b8d
parkdoyeon <parkdoyeon3239@gmail.com>
2022-10-04 23:28:38 +0900

    ILLEGAL_STATE_EXCEPTION 추가

--- src/main/java/com/example/todolist/exception/ErrorCode.java
+++ src/main/java/com/example/todolist/exception/ErrorCode.java
@@ -8,7 +8,7 @@ public enum ErrorCode {
     BAD_REQUEST(400,"Bad Request : 잘못된 입력입니다."),
     HTTP_MEDIA_TYPE_NOT_SUPPORTED_EXCEPTION(415, "BAD_REQUEST : 잘못된 입력 타입입니다."),
     HTTP_MESSAGE_NOT_READABLE_EXCEPTION(400, "BAD_REQUEST : 값이 NULL값입니다."),
-    METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION(400,"잘못된 엔드포인트 입니다.");
+    METHOD_ARGUMENT_TYPE_MISMATCH_EXCEPTION(400,"잘못된 엔드포인트 입니다."),
     ILLEGAL_STATE_EXCEPTION(400,"BAD_REQUEST : 중복된 아이디 입니다.");
 
     private final Integer status;
